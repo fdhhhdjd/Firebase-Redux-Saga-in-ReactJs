eeb99a6d7432499c092424f9141f70fb7e44d34f
tai <nguyentientai10@gmail.com>
2021-09-15 17:14:24 +0700

    add types

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,4 +1,5 @@
 import * as types from "./actionTypes";
+//! Get from Firebase
 export const getContactsStart = () => ({
   type: types.GET_CONTACTS_START,
 });
@@ -8,5 +9,17 @@ export const getContactsSuccess = (contacts) => ({
 });
 export const getContactsFail = (error) => ({
   type: types.GET_CONTACTS_FAIL,
+  payload: error,
+});
+//! Delete From Firebase
+export const deleteContactsStart = () => ({
+  type: types.DELETE_CONTACTS_START,
+});
+export const deleteContactsSuccess = (id) => ({
+  type: types.DELETE_CONTACTS_SUCCESS,
+  payload: id,
+});
+export const deleteContactsFail = (error) => ({
+  type: types.DELETE_CONTACTS_FAIL,
   payload: error,
 });
--- src/redux/store.js
+++ src/redux/store.js
@@ -7,14 +7,13 @@ import rootSaga from "./Saga";
 import rootReducer from "./root-reducer";
 import createSagaMiddleware from "@redux-saga/core";
 const sagaMiddleware = createSagaMiddleware();
-const middleware = [sagaMiddleware];
+const middleware = [sagaMiddleware]; //!saga
 if (process.env.NODE_ENV === "development") {
   middleware.push(logger);
 }
-// const store = createStore(
-//   rootReducer,
-//   composeWithDevTools(applyMiddleware(...middleware))
-// );
-const store = createStore(rootReducer, applyMiddleware(...middleware));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
 sagaMiddleware.run(rootSaga); //!saga
 export default store;
